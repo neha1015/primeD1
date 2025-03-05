566acb6b032c96f58cff956e3c807a15c8135d99
Neha Gupta <151386051+neha1015@users.noreply.github.com>
2025-03-05 21:30:24 +0530

    Add files via upload

---
+++ js_day1/script.js
@@ -0,0 +1,42 @@
+// // let const var
+// var a = "hello";
+// var b = "hello";
+// var c = 10;
+// var d = "10";
+// var e = true;
+
+// if (a === b) {
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+// if (b === c) {
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+// if (b === d) {
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+// if (c === d) {
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+// var, let, const
+
+console.log(a);
+var a = "Hello";
+
+console.log(a);
+var a = "hi";
+
+console.log(a);
+
+console.log(abc);
